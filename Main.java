db26001000b4514e2121786e6b6b629c695f23dd
Sydney Pearce <spearc3@students.towson.edu>
2021-12-11 16:01:53 -0500

    Added UI

--- Main.java
+++ Main.java
@@ -1,33 +1,40 @@
+import java.util.Scanner;
 
-
-/**
- * Main class sets up and runs the simulation,
- *
- */
 public class Main {
 	
 	static int TIME;
 	
 	public static void main( String[] args ) {
-		if (args.length != 2) {
+		if (args.length != 0) {
 			Externals.invalidUsageExit();
 		}
 		
-		String simName1 = args[0].trim();
+		//String simName1 = args[0].trim();
 		MemorySimulatorBase sim1 = null;
-		
-		if ( "first".equals(simName1) ) {
+
+		System.out.println("============================================");
+		System.out.println("Choose You Memory Simulator:");
+		System.out.println("============================================");
+		System.out.println("first" +
+		"\nbest" +
+		"\nnext" +
+		"\nworst" );
+
+		Scanner scanner = new Scanner(System.in);
+
+		String userInput = scanner.nextLine();
+		if ( "first".equals(userInput) ) {
 			sim1 = new FirstFitMemorySimulator( );
-		} else if ( "best".equals(simName1) ) {
-			sim1 = new BestFitMemorySimulator();
-		} else if ( "next".equals(simName1) ) {
+		} else if ( "best".equals(userInput) ) {
+		 	sim1 = new BestFitMemorySimulator( );
+		 } else if ( "next".equals(userInput) ) {
 			sim1 = new NextFitMemorySimulator();
-		} else if ( "worst".equals(simName1) ) {
+		} else if ( "worst".equals(userInput) ) {
 			sim1 = new WorstFitMemorySimulator();
 		}
-			else {
-			Externals.invalidUsageExit();
-		}
+	 	else {
+		 	Externals.invalidUsageExit();
+		 }
 	
 		sim1.timeStepUntil(0);
 		sim1.printMemory();
@@ -40,30 +47,5 @@ public class Main {
 		System.out.println("No more events to process... ending this simulation!");
 		
 		
-		String simName2 = args[1].trim();
-		MemorySimulatorBase sim2 = null;
-		
-		if ( "first".equals(simName2) ) {
-			sim2 = new FirstFitMemorySimulator(sim1.main_memory, sim1.copy );
-		} else if ( "best".equals(simName2) ) {
-			sim2 = new BestFitMemorySimulator(sim1.main_memory, sim1.copy );
-		} else if ( "next".equals(simName2) ) {
-			sim2 = new NextFitMemorySimulator(sim1.main_memory, sim1.copy );
-		} else if ( "worst".equals(simName2) ) {
-			sim2 = new WorstFitMemorySimulator(sim1.main_memory, sim1.copy );
-		}
-			else {
-			Externals.invalidUsageExit();
-		}
-		
-		sim2.timeStepUntil(0);
-		sim2.printMemory();
-	
-		int count2 = 0;
-		while (sim2.processesRemaining() != 0) {
-			sim2.timeStepUntil(count2++);
-		}
-		
-		System.out.println("No more events to process... ending this simulation!");
 	}
 }
